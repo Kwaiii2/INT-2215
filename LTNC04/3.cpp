d2918e00c62e16cce37975c8d19d21af390266f7
nguyen pham dung <nhitladap@gmail.com>
2024-05-20 11:27:52 +0700

    them bai tap

---
+++ LTNC04/3.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+int palindromeIndex(std::string s)
+{
+  int res, i = 0, j = s.size() - 1;
+  while (i <= j)
+  {
+    if (s[i] != s[j])
+    {
+      res = i;
+      int left_index = i;
+      int right_index = j;
+      i++;
+      while (i <= j)
+      {
+        if (s[i] != s[j])
+        {
+          res = -100;
+          break;
+        }
+        i += 1;
+        j -= 1;
+      }
+      if (res != -100) return res;
+      
+      i = left_index;
+      j = right_index - 1;
+      while (i <= j)
+      {
+        if (s[i] != s[j]) return -1;
+        i++;
+        j--;
+      }
+      return right_index;
+    }
+    i++;
+    j--;
+  }
+  return -1;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        int result = palindromeIndex(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
---
+++ LTNC04/4.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n, q; cin >> n >> q; vector<vector<int>> a;
+    for (int i = 0; i < n; i++) {
+        int k; cin >> k;
+        vector<int> tokens;
+        for (int j = 0; j < k; j++) {
+            int token; cin >> token;
+            tokens.push_back(token);
+        }
+        a.push_back(tokens);
+    }
+    for (int i = 0; i < q; i++) {
+        int x, y; cin >> x >> y;
+        cout << a[x][y] << endl;
+    }
+    return 0;
+}
